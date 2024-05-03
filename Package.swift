015db928458993e4fbcfeb09dd6b79340c61db0f
John Holtzworth <johnwhh@icloud.com>
2024-05-02 20:07:49 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,20 @@
+// swift-tools-version: 5.10
+
 import PackageDescription
 
 let package = Package(
-  name: "Pitchy"
+  name: "Pitchy",
+  platforms: [.iOS(.v13)],
+  products: [
+      .library(
+          name: "Pitchy",
+          targets: ["Pitchy"]
+      ),
+  ],
+  targets: [
+      .target(
+          name: "Pitchy",
+          dependencies: []
+      ),
+  ]
 )
